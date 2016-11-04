a46d13e9500f4aaca18c1324b9740d7d7ad3f2bb
Nghia Tran <Nghia.Tran@mgm-tp.com nghia.tran@mgm-tp.com>
2016-11-04 17:16:58 +0700

    new project node

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+var app = express();
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', {
+    title: 'Express',
+    env: app.get('env')
+  });
+});
+
+module.exports = router;
